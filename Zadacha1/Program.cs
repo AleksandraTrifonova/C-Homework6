49c0b933407c365ba6b7224e8542ec18cc514ac2
Aleksandra <s-bass11@yandex.ru>
2022-06-02 09:59:30 +0300

    Zadacha1

---
+++ Zadacha1/Program.cs
@@ -0,0 +1,34 @@
+﻿// Показать двумерный массив размером m×n заполненный вещественными числами
+void FillArray(double[,] matr)
+{
+    Random random = new Random();
+    for (int i =0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = random.NextDouble() * 10; 
+        }  
+    }
+} 
+void PrintArray(double[,] matr)
+{
+    for (int i =0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+    Console.WriteLine();    
+    }
+} 
+
+Console.WriteLine("Введите количество строк (m): ");
+int m = int.Parse(Console.ReadLine()??"0");
+Console.WriteLine("Введите количество столбцов (n): ");
+int n = int.Parse(Console.ReadLine()??"0");
+
+double[,] matrix = new double [m,n];
+
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
